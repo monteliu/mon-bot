f18b451c265e7caf9cce31b0a17cb673949519db
monteliu <monte.liu1@gmail.com>
2018-01-28 16:56:36 +0800

    Update app.py

--- app.py
+++ app.py
@@ -67,7 +67,7 @@ def handle_message(event):
     msg = 'test'
     image = 'https://dl.airtable.com/r7pvuVjRSKiGjLvU3GBb_%E6%93%B7%E5%8F%96A.PNG'
     #bot.reply_message(event.reply_token, TextSendMessage(text=msg))
-    bot.push_message(event.user_id,TextSendMessage(text=msg))
+    bot.push_message(event.source.userid,TextSendMessage(text=msg))
     ##bot.reply_message(event.reply_token, ImageMessage(original_content_url=image,preview_image_url=image))
 
 
