32001bd5c80e5b2938a36ea867a92fac17fdd3c1
mengchi <mengchi@femco.com.tw>
2018-01-29 10:19:38 +0800

    test

--- app.py
+++ app.py
@@ -26,7 +26,7 @@ imgur = None
 def _post(endpoint, **json):
     try:
         print(endpoint)
-        print('debug [%s] [%s]' % (r.status_code, json['message']))
+        print('debug [%s]' % (json))
         #r = requests.post(server_url + endpoint, json=json, timeout=30)
         
         #print('debug [%s] [%s]' % (r.status_code, json['message'])) #用來檢測heroku沒有將內容傳送過來的問題 ...吃字
