e19de965af72beb9b78ef61a29af1d052bf60579
monteliu <monte.liu1@gmail.com>
2018-02-05 21:01:57 +0800

    Update app.py

--- app.py
+++ app.py
@@ -187,7 +187,7 @@ def handle_message(event):
 @handler.add(MessageEvent, message=StickerMessage)
 def handle_sticker(event):
     _post('/sticker', **get_id(event))
-
+    print(event) 
 
 @handler.add(MessageEvent, message=ImageMessage)
 def handle_image(event):
